b7fb10dd25abac1a4a9e10ec7c838113ef208cdc
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-10-13 20:37:23 +0300

    Add files via upload

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,16 @@
+class Calculator:
+    
+    calculation_type = "Arithmetic Operations"
+    
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a, b):
+        cls.a = a
+        cls.b = b
+        
+        print(f"Calculation type: {cls.calculation_type}")
+        
+        return cls.a * cls.b
