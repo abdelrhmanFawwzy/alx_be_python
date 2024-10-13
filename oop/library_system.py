21e8aea3f0c889fc6bbf8ca125c1d3a3e6e92817
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-10-13 20:43:14 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,6 +3,9 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+    def __str__(self):
+        return f""
         
 class EBook(Book):
     
